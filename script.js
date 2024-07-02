8a90c646cfce4b97ffa5134f1fad929311c08e96
deividas strole <deiwis@yahoo.com>
2024-07-01 20:13:37 -0700

    challenge

--- script.js
+++ script.js
@@ -1,15 +1,44 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const x = 23;
-if (x === `3`) console.log(`kuku`);
+// const x = 23;
+// if (x === `3`) console.log(`kuku`);
 
-function bybys() {
-  return null;
-}
+// function bybys() {
+//   return null;
+// }
 
-const calcAge = (birthYear) => 2024 - birthYear;
+// const calcAge = (birthYear) => 2024 - birthYear;
 
-console.log(calcAge(1990));
+// console.log(calcAge(1990));
 
-console.log();
+// console.log(calcAge);
+
+// const mKelvin = function () {
+//   const measurement = {
+//     type: 'temp',
+//     unit: 'celsius',
+//     value: Number(prompt('Degree celsius:')),
+//   };
+
+//   console.table(measurement);
+
+//   const kelvin = measurement.value + 273;
+//   return kelvin;
+// };
+
+// console.log(mKelvin());
+
+const testDataArr1 = [17, 21, 23];
+const testDataArr2 = [12, 5, -5, 0, 4];
+
+let fullString = '... ';
+
+const printForcast = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    fullString += arr[i] + `°C in ${i + 1} days ... `;
+  }
+  return fullString;
+};
+
+console.log(printForcast(testDataArr2));
